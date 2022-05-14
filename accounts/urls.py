6068454572950d0ef68ee3baeecdfb52d8278163
HumbleResilience <emoore.tech@gmail.com>
2022-05-14 11:08:12 -0500

    changed profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
-from .views import SignUpView, ManageAccountView, PasswordChangePageView
+from django.contrib.auth import views as auth_views
+from .views import ManageAccountView, PasswordChangePageView, RegisterView, CustomLoginView
+from accounts.forms import LoginForm
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
+    # path('signup/', SignUpView.as_view(), name='signup'),
     path('manage_account/', ManageAccountView.as_view(), name='manage'),
     path('password/', PasswordChangePageView.as_view(), name="change-password"),
-]
+    path('register/', RegisterView.as_view(), name = 'register'),
+    path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name = 'registration/login.html'))
+] 
+
+
+
--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from.views import AboutPageView, CheckoutPageView, ContactPageView, HomePageView,  LoginPageView,LogoutPageView, PasswordChangePageView, CatalogPageView, CheckoutPageView
+from.views import AboutPageView, CheckoutPageView, ContactPageView, HomePageView,LogoutPageView, PasswordChangePageView, CatalogPageView, CheckoutPageView
 
 
 
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
     path('about/', AboutPageView.as_view(), name= "about"),
     path('contact/', ContactPageView.as_view(), name="contact"),
-    path('registration/login/', LoginPageView.as_view(), name="login" ),
+  
     path('logged_out/', LogoutPageView.as_view(), name="logout"),
     path('password_change/', PasswordChangePageView.as_view(), name="password_change"),
     path('catalog/', CatalogPageView.as_view(), name="catalog"),
